202b88248d0a7cd6b8e658a16235c88e444e66eb
sozi <sozyarnadir@gmail.com>
2022-10-24 21:54:00 +0300

    Assignment Completed

--- src/components/ColorList.js
+++ src/components/ColorList.js
@@ -1,26 +1,35 @@
 import React from "react";
 
-function ColorList() {
-  const colors = [
-    "firebrick",
-    "rebeccapurple",
-    "salmon",
-    "darkslategray",
-    "hotpink",
-  ];
+function ColorItem(props) {
+    return <li style = {
+        { color: props.color }
+    } > { props.color } < /li>
+}
 
-  return (
-    <div>
-      <h1>Top 5 CSS Colors</h1>
-      <ol>
-        <li style={{ color: colors[0] }}>{colors[0]}</li>
-        <li style={{ color: colors[1] }}>{colors[1]}</li>
-        <li style={{ color: colors[2] }}>{colors[2]}</li>
-        <li style={{ color: colors[3] }}>{colors[3]}</li>
-        <li style={{ color: colors[4] }}>{colors[4]}</li>
-      </ol>
-    </div>
-  );
+function ColorList() {
+    const colors = [
+        "firebrick",
+        "rebeccapurple",
+        "salmon",
+        "darkslategray",
+        "hotpink",
+    ];
+
+    const colorElements = colors.map((color) => {
+        return <ColorItem key = { color }
+        color = { color }
+        />
+    })
+    return ( <
+        div >
+        <
+        h1 > Top 5 CSS Colors < /h1> <
+
+        ol > { colorElements } <
+        /ol> < /
+        div >
+    );
+
 }
 
 export default ColorList;
